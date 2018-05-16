2430754a0267621e9fa3755568974558d0efb50e
Homum Ahsan <hahsan@uchicago.edu>
2018-05-16 11:59:40 -0500

    finished initial board

--- game/index.js
+++ game/index.js
@@ -1,6 +1,25 @@
 import {Map} from 'immutable'
 
-export default function reducer(state, action) {
+let board = Map()
+
+export default function reducer(state = { turn: 'X', board: board }, action) {
   // TODO
-  return state
+  switch(action.type) {
+    case "START":
+      return state
+    case "MOVE":
+      let newTurn = 'X'
+      if (action.player === 'X') {
+        newTurn = 'O'
+      } 
+      return { turn: newTurn, board: state.board.setIn(action.position, action.player) }
+    default:
+      return state
+  }
 }
+
+export const move = (turn, position) => {
+  return ( {type: "MOVE", position: position, player: turn} )
+}
+
+export const bad = () => {}
